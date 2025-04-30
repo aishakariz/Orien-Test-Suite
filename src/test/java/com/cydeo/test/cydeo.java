75728e97941e0eb3154f924d96f6b2427b4cccb9
aisha <aishaemin33@gmail.com>
2025-04-29 21:49:11 -0400

    night-study-group-first-project

---
+++ src/test/java/com/cydeo/test/cydeo.java
@@ -0,0 +1,19 @@
+package com.cydeo.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.Driver;
+
+    public class cydeo {
+    public static void main(String[] args) {
+
+
+       WebDriver driver= new ChromeDriver();
+
+      driver.get("https://www.google.com/");
+
+
+
+    }
+}
