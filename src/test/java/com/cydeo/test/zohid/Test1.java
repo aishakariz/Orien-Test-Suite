3d3bcefffda0308eb841720298d467c5571fc0cb
atiuz90gmailcom <atiuz90@gmail.com>
2025-04-30 13:47:14 -0400

    New member joined

---
+++ src/test/java/com/cydeo/test/zohid/Test1.java
@@ -0,0 +1,8 @@
+package com.cydeo.test.zohid;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hey guys I guess, I joined to night study project ");
+    }
+}
