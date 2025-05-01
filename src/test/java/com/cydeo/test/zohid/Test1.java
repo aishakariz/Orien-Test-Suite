6c9b63fe2cf2618ce8ca17fd078fdd235c9ade37
atiuz90gmailcom <atiuz90@gmail.com>
2025-05-01 13:02:19 -0400

    New member joined

--- src/test/java/com/cydeo/test/zohid/Test1.java
+++ src/test/java/com/cydeo/test/zohid/Test1.java
@@ -3,7 +3,7 @@ package com.cydeo.test.zohid;
 public class Test1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello Hello");
 
     }
 }
