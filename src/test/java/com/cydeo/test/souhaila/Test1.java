39afc28d28567cf112f585967138eff462e97639
souha <souhailabenzahra95@gmail.com>
2025-05-02 20:41:20 -0400

    First commit test

---
+++ src/test/java/com/cydeo/test/souhaila/Test1.java
@@ -0,0 +1,11 @@
+package com.cydeo.test.souhaila;
+
+public class Test1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Team!!! \nHave a productive week-end!!!");
+
+
+
+    }
+}
