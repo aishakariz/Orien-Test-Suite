1f4f549d80f8f3b3429aaa31f45f4fd137aa1b58
Emmanuelduvalsaint <emduvalsaint@gmail.com>
2025-05-15 23:03:11 -0400

    Task1 and UtilitiesClass added

---
+++ src/test/java/com/cydeo/Utilities/BrowserUtils.java
@@ -0,0 +1,52 @@
+package com.cydeo.Utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import java.util.Set;
+
+/*
+This class will be storing only the utility methods that can be used across the project.
+ */
+public class BrowserUtils {
+    /*
+       This method will accept int (in seconds)
+       and execute Thread.sleep method for given duration
+       Arg: int second
+        */
+    public static void sleep(int second) {
+        second *= 1000;
+        try {
+            Thread.sleep(second);
+        } catch (InterruptedException e) {
+
+        }
+    }
+
+    public static void switchWindowAndVerify(WebDriver driver, String expectedInUrl, String expectedInTitle) {
+        //Return and store all window handles in a Set.
+        Set<String> allWindowHandles = driver.getWindowHandles();
+
+        for (String each : allWindowHandles) {
+
+            driver.switchTo().window(each);
+            System.out.println("Current URL: " + driver.getCurrentUrl());
+
+            if (driver.getCurrentUrl().contains(expectedInUrl)) {
+                break;
+            }
+        }
+
+        //5. Assert:Title contains “Etsy”
+        String actualTitle = driver.getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
+    }
+
+    public static void verifyTitle(WebDriver driver, String expectedInTitle){
+        Assert.assertEquals(driver.getTitle(), expectedInTitle);
+    }
+    public static void verifyTitleContains(WebDriver driver, String expectedInTitle){
+        Assert.assertTrue(driver.getTitle().contains(expectedInTitle));
+    }
+}
+
