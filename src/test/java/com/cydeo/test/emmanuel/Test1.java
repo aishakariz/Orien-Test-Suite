c139319b1fb9593dfbc6e64d4972618c20f36b45
Emmanuelduvalsaint <emduvalsaint@gmail.com>
2025-05-15 20:56:05 -0400

    New project name

--- src/test/java/com/cydeo/test/emmanuel/Test1.java
+++ src/test/java/com/cydeo/test/emmanuel/Test1.java
@@ -3,6 +3,7 @@ package com.cydeo.test.emmanuel;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello everyone, I hope you are doing well");
+        System.out.println("New project name");
 
     }
 }
